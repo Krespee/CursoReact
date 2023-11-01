a395dbcf4e2f3440875f3d3f9693f6184ce3c339
Nayn <naynzahir@gmail.com>
2023-11-01 06:37:43 -0300

    Fragment

--- src/primerComponente.jsx
+++ src/primerComponente.jsx
@@ -2,13 +2,20 @@ const string = "Esto es un texto";
 const number = 154545;
 const array = ["hola", "Mundo", 1, 2023];
 const boolean = true;
-const objeto = {nombre:"Nayn", edad:22};
-const funcion = () => 1+1;
+const objeto = { nombre: "Nayn", edad: 22 };
+const funcion = () => 1 + 1;
 const fecha = new Date();
-funcion()
+funcion();
 
 export const PrimerComponente = () => {
   return (
-    <h1>{JSON.stringify(fecha)}</h1>
-  )
-}
+    <>
+      <h1>Variables en jsx</h1>
+      <h4>Variables tipo String:</h4> <p>{string}</p>
+      <h4>Variables tipo number:</h4> <p>{number}</p>
+      <h4>Variables tipo array:</h4> <p>{array}</p>
+      <h4>Variables tipo boolean:</h4> <p>{boolean}</p>
+      <h4>Variables tipo funcion:</h4> <p>{funcion}</p>
+    </>
+  );
+};
