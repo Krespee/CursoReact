a22c0d18c3682f6bad9e68ebde869633014b27d2
Nayn <naynzahir@gmail.com>
2023-11-01 06:00:20 -0300

    Primer Componente

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { PrimerComponente } from './primerComponente'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PrimerComponente/>
+  </React.StrictMode>,
+)
