fdd9a11003b38bb7775afb9d2471f114512f9cd3
Nayn <naynzahir@gmail.com>
2023-11-01 06:31:38 -0300

    variables

--- src/primerComponente.jsx
+++ src/primerComponente.jsx
@@ -1,5 +1,14 @@
+const string = "Esto es un texto";
+const number = 154545;
+const array = ["hola", "Mundo", 1, 2023];
+const boolean = true;
+const objeto = {nombre:"Nayn", edad:22};
+const funcion = () => 1+1;
+const fecha = new Date();
+funcion()
+
 export const PrimerComponente = () => {
   return (
-    <h1>primerComponente</h1>
+    <h1>{JSON.stringify(fecha)}</h1>
   )
 }
