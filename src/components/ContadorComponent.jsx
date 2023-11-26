0a950725f1e2efb49c537e7a87514d88fbcd6458
Nayn <naynzahir@gmail.com>
2023-11-26 03:18:16 -0300

    Contador desde cero más Boostrap

---
+++ src/components/ContadorComponent.jsx
@@ -0,0 +1,14 @@
+import { useConter } from "../hooks/useConter"
+
+export const ContadorComponent = () => {
+    const {contador, incrementar, decrementar, resetear} = useConter(10)
+
+  return (
+    <>
+    <h1>Contador: {contador} </h1>
+    <button className="btn btn-primary" onClick={()=> incrementar(1)}>+1</button>
+    <button className="btn btn-danger" onClick={()=> resetear()}>Reset</button>
+    <button className="btn btn-primary" onClick={()=> decrementar(1, false)}>-1</button>
+    </>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import "./styles.css"
-import { UserApp } from './UserApp'
+import { HooksApp } from './HooksApp'
+import "./index.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <UserApp/>
+    <HooksApp></HooksApp>
   </React.StrictMode>,
 )
