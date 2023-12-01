12e5ae343d80a18a3563359d17c09aa6c41e8496
Nayn <naynzahir@gmail.com>
2023-12-01 03:46:49 -0300

    useCallBack & React.memo

--- src/HooksApp.jsx
+++ src/HooksApp.jsx
@@ -1,4 +1,5 @@
 import { CalculosPesados } from "./components/CalculosPesados"
+import { CallBackComponent } from "./components/CallBackComponent"
 import { FormularioComponent } from "./components/FormularioComponent"
 import { UserComponents } from "./components/UserComponents"
 export const HooksApp = () => {
@@ -6,7 +7,7 @@ export const HooksApp = () => {
     <>
     <h1>Aplicacion de Hooks</h1>
     <hr />
-    <CalculosPesados></CalculosPesados>
+    <CallBackComponent></CallBackComponent>
     </>
   )
 }
