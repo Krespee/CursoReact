dbb1fcd243bf0ab8d787c77bc412bd9bd7643c6e
Nayn <naynzahir@gmail.com>
2024-01-15 19:04:34 -0300

    finalizando utilizacion de useReducer

--- src/components/ListaTareas.jsx
+++ src/components/ListaTareas.jsx
@@ -1,5 +1,8 @@
+import { useReducer } from "react";
+import { useForm } from "../hooks/useForm";
+
 const initalState = [{
-    id:1,
+    id: new Date().getTime(),
     tarea: "Explicar Reducers",
     finalizada: false
 }];
@@ -10,14 +13,7 @@ const nuevaTarea = {
     finalizada: false
 }
 
-const agrearTarea = {
-    type: "[TAREAS] Agregar Tarea",
-    payload: nuevaTarea
-}
-const editarTarea = {
-    type: "[TAREAS] Editar Tarea",
-    payload: nuevaTarea
-}
+
 const eliminarTarea = {
     type: "[TAREAS] Eliminar Tarea",
 }
@@ -31,34 +27,120 @@ const tareaReducer = (state = initalState, action = {})=>{
     case "[TAREAS] Agregar Tarea":
         return [...state, action.payload]
 
-    case "[TAREAS] Editar Tarea":
-        for (const key of state) {
-            if (key.id == action.payload.id) {
-                
-            }else{
-                console.log("No Se encontro el elemento a editar");
+    case "[TAREAS] Finzalizar Tarea":
+        return state.map(tarea => {
+            if (tarea.id === action.payload) {
+                return{
+                    ...tarea,
+                    finalizada: !tarea.finalizada
+                }
             }
-        }
+            return tarea
+        })
     
     case "[TAREAS] Eliminar Tarea":
-        return [...state, action.payload]
+        return state.filter (tarea => tarea.id != action.payload)
 
     case "[TAREAS] Borrar Tarea":
-        return [...state, action.payload]
+        return []
    
     default:
-        break;
+        return state
    }
-    return state
 }
 
-console.log(tareaReducer(initalState, editarTarea));
+// console.log(tareaReducer(initalState, editarTarea));
 
 
 
 
 export const ListaTareas = () => {
+
+    const [state, dispatch] = useReducer(tareaReducer, initalState)
+
+    const {tarea, formState, onInputChange} = useForm({tarea :""})
+
+    const agrearTarea = (e) =>{
+        e.preventDefault()
+        if (formState.tarea == "") return
+        console.log(formState);
+        const tarea = {
+            id: new Date().getTime(),
+            tarea: formState.tarea,
+            finalizada:false
+        }
+        const action = {
+         type: "[TAREAS] Agregar Tarea",
+         payload: tarea
+        }
+        dispatch(action)
+    }
+
+    const marcarFinalizada = ({id}) =>{
+
+        const action = {
+            type: "[TAREAS] Finzalizar Tarea",
+            payload: id
+        }
+
+        dispatch(action)
+    }
+
+    const eliminarTarea = ({id}) =>{
+        const action = {
+            type: "[TAREAS] Eliminar Tarea",
+            payload: id
+        }
+
+        dispatch(action)
+    }
+
+    const reset = () =>{
+        const action = {
+            type: "[TAREAS] Borrar Tarea",
+            payload: ""
+        }
+        dispatch(action)
+    }
   return (
-    <div>ListaTareas</div>
+    <>
+        <form onSubmit={agrearTarea}>
+            <div className="form-group">
+             <input 
+             type="text" 
+             className="form-control" 
+             id="tarea" 
+             name="tarea" 
+             placeholder="Ingresa Tarea"
+             value={tarea}
+             onChange={onInputChange}
+             />
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+            <button type="button" className="btn btn-primary" onClick={reset}>Reset</button>
+        </form>
+
+        <hr />
+        <ul className="list-group">
+            {state.map(item =>{ 
+                return(
+                    
+                <li className="list-group-item d-flex justify-content-between" key={item.id}>
+                    <span>{item.tarea}</span>
+                    <input 
+                        type="checkbox" 
+                        value={item.finalizada}
+                        onChange={() => marcarFinalizada(item)}
+                    />
+                    <button
+                    className="btn btn-danger"
+                    onClick={()=>eliminarTarea(item)}
+                    >Borrar</button>
+                    {item.finalizada ? "✅" : "❌"}
+                    </li>
+                )
+            })}
+        </ul>
+    </>
   )
 }
